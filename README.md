9d1ba2c80e57f1e03430ab7b55b76d79f99d5a00
Arik <arikabdullah1122@gmail.com>
2022-04-22 14:53:39 -0400

    Still trying to figure out the difference in this code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Learn-Python-
-This will be a collection of my projects while I learn python.
+
+This will be a collection of my projects while I learn python. I will continue to update this repo as I practice using Git and coding in the Python language.
--- dif_pi_calc.py
+++ dif_pi_calc.py
@@ -1,64 +1,58 @@
-#!/usr/bin/env python3
+# Attempt to understand different functions of calculating
 
-# Find PI to the Nth Digit
-# Have the user enter a number 'n'
-# and print out PI to the 'n'th digit
+def calcpi(limit): #establishes a named function to calculate pi to a certain number of decimal places
 
+  
+  q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
 
-def calcPi(limit):  # Generator function
-    """
-    Prints out the digits of PI
-    until it reaches the given limit
-    """
+  decimal = limit
+  counter = 0
 
-    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+  while counter != decimal + 1:
+    if 4 * q + r - t < n * t:
+      #yield a digit
+      yield n
 
-    decimal = limit
-    counter = 0
+      #placing a decimal
+      if counter == 0:
+        yield "."
+      #end of first step
+      if decimal == counter:
+        print('')
+        break
+      counter += 1
+      nr = 10 * (r -n * t)
+      n = ((10 * (3 * q + r)) // t) - 10 * n
+      q *= 10
+      r = nr
 
-    while counter != decimal + 1:
-            if 4 * q + r - t < n * t:
-                    # yield digit
-                    yield n
-                    # insert period after first digit
-                    if counter == 0:
-                            yield '.'
-                    # end
-                    if decimal == counter:
-                            print('')
-                            break
-                    counter += 1
-                    nr = 10 * (r - n * t)
-                    n = ((10 * (3 * q + r)) // t) - 10 * n
-                    q *= 10
-                    r = nr
-            else:
-                    nr = (2 * q + r) * l
-                    nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
-                    q *= k
-                    t *= l
-                    l += 2
-                    k += 1
-                    n = nn
-                    r = nr
+    else:
+      nr = (2 * q + r) * l
+      nn = (q * (7 * k) + 2 + (r * 1)) // (t * 1)
+      q *= k
+      t *= l
+      l += 2
+      k += 1
+      n = nn
+      r = nr
 
+def main (): #wrapper function
 
-def main():  # Wrapper function
+  #Calls calcpi with a given limit
+  pi_digits = calcpi(int(input(
+    "Enter however many decimals you would like pi to calculated to")))
 
-    # Calls CalcPi with the given limit
-    pi_digits = calcPi(int(input(
-        "Enter the number of decimals to calculate to: ")))
+  i = 0
 
-    i = 0
+  #Prints output of calcpi function
+  #Inserts a newline after every 40th number
 
-    # Prints the output of calcPi generator function
-    # Inserts a newline after every 40th number
-    for d in pi_digits:
-            print(d, end='')
-            i += 1
-            if i == 40:
-                print("")
-                i = 0
+  for d in pi_digits:
+    print(d, end = '')
+    i += 1
+    if i == 40:
+      print("")
+      i = 0
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__' :
+  main()
